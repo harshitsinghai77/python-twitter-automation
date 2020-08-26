e7ac0aa74d5d4bccfe5012d78453c60a1c29c42f
harshitsinghai77 <harshitsinghai77@gmail.com>
2020-08-27 00:53:03 +0530

    Cleaned code

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
-CONSUMER_KEY = 'cCc7DXRFOSf54Q3sfCTJAwqNr'
-CONSUMER_SECRET = 'HzWSQS6ZIZip7Q3EeYpbZz6uRH6m11V7mpG0ipcnBUjYfa3Q00'
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
 
-REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
-ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
-AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
+REQUEST_TOKEN_URL = ''
+ACCESS_TOKEN_URL = ''
+AUTHORIZATION_URL = ''
