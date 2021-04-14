24981d6639128c7d2daf16bdf8fc678ab936c39b
Harshit Singhai <harshitsinghai77@gmail.com>
2021-04-14 16:08:49 +0530

    Changed

--- my_demo_file.py
+++ my_demo_file.py
@@ -1,10 +1,11 @@
 import git
 
 branch="feature-branch"
+rebase_branch="feature-branch-2"
 
 g = git.Git()
 g.fetch("origin", branch)
 g.checkout(branch)
 
 g.fetch("origin", "master")
-g.rebase("origin/master")
+g.rebase(f"origin/{rebase_branch}")
