ac053fad87673e8d1cbe9028af17ee9764737df8
Harshit Singhai <harshitsinghai77@gmail.com>
2021-04-23 11:19:01 +0530

    rebase with original

--- my_random.py
+++ my_random.py
@@ -1,16 +1,15 @@
 import argparse
+
 import aiohttp
 import asyncio
-
 import git
-
-# OWNER = "deepsourcelabs"
-# DEFAULT_REPO = "asgard"
-# BASE_DIR = "core/migrations/"
 
 OWNER = "harshitsinghai77"
 DEFAULT_REPO = "python-twitter-automation"
-BASE_DIR = "my_folder/"
+DEFAULT_MASTER_BRANCH = "master"
+SPOCK_EMAIL = "harshitsinghai77@gmail.com"
+SPOCK_NAME = "harshitsinghai77"
+BASE_DIR = "my_demo_file.py"
 
 BASE_URL = "https://api.github.com/"
 params = {"state": "open", "per_page": 100}
@@ -25,33 +24,31 @@ async def fetch(url, session, branch_name=None):
         return await resp.json(), resp.status
 
 
-async def post(url, comment, session):
-    """Make a post request to the given url, return response and status."""
-    async with session.post(url, json={"body": comment}, headers=headers) as resp:
-        return await resp.json(), resp.status
+# def git_set_user_config():
+#     """Set name and email config to use git."""
+#     repo = git.Repo()
+#     repo.config_writer().set_value("user", "name", SPOCK_NAME).release()
+#     repo.config_writer().set_value("user", "email", SPOCK_EMAIL).release()
 
 
 def rebase_branch_with_master(branch_name):
-    
-    # branch="feature-branch"
-    master_branch="master"
+    """Fetch the branch from origin, checkout the branch and rebase it with master."""
+    try:
+        g = git.Git()
+        g.fetch("origin", branch_name)
+        g.checkout(branch_name)
 
-    g = git.Git()
-    g.fetch("origin", branch_name)
-    g.checkout(branch_name)
+        g.fetch("origin", DEFAULT_MASTER_BRANCH)
+        g.rebase(f"origin/{DEFAULT_MASTER_BRANCH}")
+    except git.exc.GitError as e:
+        print("Some error occured while rebasing ", branch_name, e)
+    finally:
+        g.push(force=True)
 
-    g.fetch("origin", "master")
-    g.rebase(f"origin/{master_branch}")
-
-def switch_to_original_branch():
-    default_branch = "feature-branch"
-
-    g = git.Git()
-    g.checkout(default_branch)
 
 async def main(pat: str):
     """pat: personal access token."""
-    headers["Authorization"] = "token " + pat
+    headers["Authorization"] = f"token {pat}"
 
     async with aiohttp.ClientSession() as session:
         # get all open pull requests
@@ -68,16 +65,14 @@ async def main(pat: str):
             return
 
         # get pull request number and name of the branch
-        pull_request = [(pr["number"], pr["head"]["ref"]) for pr in all_pull_requests]
+        pull_requests = [(pr["number"], pr["head"]["ref"]) for pr in all_pull_requests]
 
         # for every open pull request get the list of files_changed in the PR. This will be async task,
         # each task will be append to a list and run concurrently. If all awaitables are completed successfully,
         # the result is an aggregate list of returned values.
         pull_request_files_changed_task = []
 
-        for pr in pull_request:
-            pr_number = pr[0]
-            branch_name = pr[1]
+        for pr_number, branch_name in pull_requests:
             file_changed_url = (
                 BASE_URL + f"repos/{OWNER}/{DEFAULT_REPO}/pulls/{pr_number}/files"
             )
@@ -88,64 +83,31 @@ async def main(pat: str):
 
         all_pr_files_changed = await asyncio.gather(*pull_request_files_changed_task)
 
-        # with open("scripts/dump1.json", "w") as json_file:
-        #     json.dump(list_pull_requests, json_file)
-
-        # If the PR changed the file inside BASE_DIR then add it the rebase_pr
-        rebase_pr = set()
+        # If the PR make any changes to the file inside BASE_DIR then add it the rebase_branch
+        rebase_branch = set()
         for pr in all_pr_files_changed:
             branch_name = pr["branch_name"]
             for files in pr["files"]:
                 file_changed = files["filename"]
                 if file_changed.startswith(BASE_DIR):
-                    rebase_pr.add(branch_name)
+                    print("branch_name for rebase", branch_name)
+                    rebase_branch.add(branch_name)
                     break
 
-        for pr in rebase_pr:
-            print("Rebasing for pr ", pr)
-            try:
-                rebase_branch_with_master(pr)
-            except:
-                print("Some error occured while rebasing ", pr)
-        
-        switch_to_original_branch()
-        # # Loop through rebase_pr and for each pr, rebase the branch by commenting `/rebase`
-        # for pr in rebase_pr:
-        #     comment_url = (
-        #         BASE_URL + f"repos/{OWNER}/{DEFAULT_REPO}/issues/{pr}/comments"
-        #     )
-        #     comment, status = await post(
-        #         url=comment_url, comment="/rebase", session=session
-        #     )
-        #     if status == 201:
-        #         print("Rebase the PR ", comment["html_url"])
-        #     else:
-        #         print("Some error occured ", comment)
-        # url = BASE_URL + f"repos/{OWNER}/{DEFAULT_REPO}/merges"
-        # result = await merge(
-        #     url=url, head="my-temp-branch", base="master", session=session
-        # )
-        # print(result)
+        # set user config for git
+        # git_set_user_config()
+
+        # rebase `rebase_branch` with master.
+        # for branch_name in rebase_branch:
+        #     print("Rebasing for branch ", branch_name)
+        #     rebase_branch_with_master(branch_name=branch_name)
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--pat", dest="pat", type=str)
-pat_key = parser.parse_args().pat
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pat", dest="pat", type=str)
+    pat_key = parser.parse_args().pat
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main(pat=pat_key))
-loop.close()
-
-# g = git.Git()
-# g.fetch("origin", "gh-action-check-migration")
-# g.checkout("gh-action-check-migration")
-
-# g.fetch("origin", "master")
-# g.rebase("origin/master")
-
-# repo = git.Repo()
-# repo.git.fetch("origin", "master")
-# repo.git.rebase("origin/master")
-
-# rebased = repo.git.rebase()
-# print(rebased)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(pat=pat_key))
+    loop.close()
